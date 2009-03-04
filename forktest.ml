05fdb882d40e439a5b03cd8979cb31b176cdf98f
Anil Madhavapeddy <anil@recoil.org>
2009-03-04 17:21:00 +0000

    fix up forktest to work with the latest fork_helper api

--- forktest.ml
+++ forktest.ml
@@ -4,11 +4,11 @@ open Printf
 let _ = 
    let ofn = print_endline in
    let efn = print_endline in
-   let t1 = create "find /Users/avsm/src/xgit" ofn efn in
-   let t2  = create "ls -laR /Users/avsm/src/git" ofn efn in
+   let t1 = create "find /Users/avsm/src/xgit" [||] "/" ofn efn in
+   let t2  = create "ls -laR /Users/avsm/src/git" [||] "/" ofn efn in
    print_endline "done main";
-   Thread.join t1.thread;
-   Thread.join t2.thread;
+   let () = match t1.thread with |Some t -> Thread.join t |None -> () in
+   let () = match t2.thread with |Some t -> Thread.join t |None -> () in
    print_endline (string_of_task t1);
    print_endline (string_of_task t2);
    ()
