1cd03230c75002e07cc7f543794c23018bfd107e
Anil Madhavapeddy <anil@recoil.org>
2009-03-10 04:42:19 +0000

    replace the header-based session management with HTTP basic auth for now, so that it'll work much more easily with normal browsers

--- lifedb_dispatch.ml
+++ lifedb_dispatch.ml
@@ -12,34 +12,18 @@ let mark_get_rpc (cgi : Netcgi.cgi_activation) =
     cgi#set_header ~cache:`No_cache ~content_type:"application/json" ()
 
 let dispatch (cgi : Netcgi.cgi_activation) =
-    let cont = Netplex_cenv.self_cont() in
     let u = Nethttp.uripath_decode (cgi#url ()) in
     let url = Neturl.parse_url u in
     let url_list = List.filter ((<>) "") (Neturl.url_path url) in
-    let url_hd = List.hd url_list in
+    let url_hd = try List.hd url_list with _ -> "" in
     let url_path = Neturl.join_path (Neturl.url_path url) in
-    cont#log `Debug (sprintf "Lifedb_dispatch: url=%s" url_path);
-    (* check for a valid session *)
-    let session = cgi#environment#input_header_field ~default:"" "session" in
-    match Lifedb_session.check_valid session with
+    Log.logdbg "Lifedb_dispatch: url=%s" url_path;
+    match check_auth cgi with
     (* not authenticated *)
-    |false -> begin
-        match cgi#request_method, url_hd with
-        |`POST, "login" ->
-            let arg = mark_post_rpc cgi in
-            Lifedb_session.dispatch cgi (`Login arg)
-        |(`HEAD|`GET), "ping" ->
-            return_error cgi `Forbidden "Invalid session" "Login before pinging"
-        |_ -> 
-            return_error cgi `Forbidden "Invalid session" "No valid session header found"
-    end
-    (* authenticated *)
+    |false -> ()
     |true -> begin
         try 
             match cgi#request_method, url_hd with
-            |`POST, "logout" -> begin
-               Lifedb_session.dispatch cgi (`Logout session)
-            end
             |(`HEAD|`GET), "ping" ->
                 cgi#output#output_string "pong";
             |`POST, "scan" ->
--- lifedb_rpc.ml
+++ lifedb_rpc.ml
@@ -11,14 +11,33 @@ type json rpc_result_error = {
 exception Invalid_rpc of string
 
 let return_error (cgi:Netcgi.cgi_activation) (code:Nethttp.http_status) error reason =
-    cgi#output#rollback_work();
-	cgi#set_header ~status:code
-	  ~cache:`No_cache 
-	  ~content_type:"application/json; charset=\"iso-8859-1\""
-	  ();
-	let resp = Json_io.string_of_json (json_of_rpc_result_error 
-	    { error=error; reason=reason }) in
-    let cont = Netplex_cenv.self_cont() in
-    cont#log `Info (sprintf "return_error: %s" resp);
-	cgi#output#output_string resp
-    
+    cgi#output#rollback_work ();
+    cgi#set_header ~status:code ~cache:`No_cache 
+        ~content_type:"application/json; charset=\"iso-8859-1\"" ();
+    let resp = Json_io.string_of_json (json_of_rpc_result_error 
+        { error=error; reason=reason }) in
+    cgi#output#output_string resp
+
+let return_need_auth (cgi:Netcgi.cgi_activation) =
+    cgi#output#rollback_work ();
+    Log.logmod "Auth" "Unauthorized request for %s" (cgi#url ());
+    cgi#set_header ~status:`Unauthorized ~cache:`No_cache ();
+    Nethttp.Header.set_www_authenticate cgi#environment#output_header ["basic", ["realm", "LifeDB credentials"]]
+
+let check_passwd username passwd =
+    username = "foo" and passwd = "bar"
+
+let check_auth cgi =
+    try begin
+        let authty, authinf = Nethttp.Header.get_authorization cgi#environment#input_header in
+        let passwd = Netencoding.Base64.decode (List.assoc "credentials" authinf) in
+        let r = match Str.bounded_split (Str.regexp_string ":") passwd 2 with
+        |[username;passwd] -> check_passwd username passwd
+        |_ -> false in
+        if not r then return_need_auth cgi;
+        r
+    end with
+        Not_found -> begin
+            return_need_auth cgi; false
+        end
+
